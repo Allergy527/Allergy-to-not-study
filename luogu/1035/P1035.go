8a800dce8c9f68387cff6d009f512120ba503806
Allergy <Allergy527@gmail.com>
2023-11-24 21:10:46 +0800

    Go

---
+++ luogu/1035/P1035.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var res float64
+	var Sn float64 = 0
+	var tmp float64 = 0
+	fmt.Scan(&res)
+	for {
+		tmp++
+		if Sn+(1.0/tmp) > res {
+			break
+		}
+		Sn += 1.0 / tmp
+	}
+	fmt.Printf("%.0f", tmp)
+}
