758f592bd3c05f2ab752c52ce2b1e56379a61b27
Allergy <Allergy527@gmail.com>
2023-11-25 15:42:00 +0800

    RUST

--- src/bin/p1046.rs
+++ src/bin/p1046.rs
@@ -1,17 +1,18 @@
 use std::io;
 fn main() {
-    let mut input = String::new();
+    let mut input: String = String::new();
+    let mut input2: String = String::new();
     io::stdin().read_line(&mut input).unwrap();
     let array: Vec<i32> = input
         .trim()
         .split_whitespace()
         .map(|s: &str| s.parse().unwrap())
         .collect();
-    io::stdin().read_line(&mut input).unwrap();
-    let capabl: i32 = input.trim().parse().unwrap();
+    io::stdin().read_line(&mut input2).unwrap();
+    let capabl: i32 = input2.trim().parse::<i32>().expect("crazy!!");
     let mut sum: i32 = 0;
     for num in array {
-        if capabl + 30 <= num {
+        if capabl + 30 >= num {
             sum += 1;
         }
     }
