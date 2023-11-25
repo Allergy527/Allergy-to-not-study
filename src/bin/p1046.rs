dccfce1ce594b0f4435beb91f3b81daf8cc663c8
Allergy <Allergy527@gmail.com>
2023-11-25 16:03:48 +0800

    RUST

--- src/bin/p1046.rs
+++ src/bin/p1046.rs
@@ -1,20 +1,19 @@
 use std::io;
 fn main() {
-    let mut input: String = String::new();
-    let mut input2: String = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let array: Vec<i32> = input
-        .trim()
+    let mut input1: String = String::new(); //用来读数组
+    let mut input2: String = String::new(); //用来读最大高度
+    let mut sum: i32 = 0;
+    io::stdin().read_line(&mut input1).unwrap();
+    let array: Vec<i32> = input1 //读取数组
+        .trim() //去掉首尾空格
         .split_whitespace()
         .map(|s: &str| s.parse().unwrap())
         .collect();
     io::stdin().read_line(&mut input2).unwrap();
-    let capabl: i32 = input2.trim().parse::<i32>().expect("crazy!!");
-    let mut sum: i32 = 0;
-    for num in array {
-        if capabl + 30 >= num {
-            sum += 1;
-        }
+    let capabl: i32 = input2.trim().parse::<i32>().expect("crazy!!") + 30;
+    //下面是处理部分
+    for _num in array.iter().filter(|x| x <= &&capabl) {
+        sum += 1;
     }
     println!("{}", sum);
 }
