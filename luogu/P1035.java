b4dc30d3671f5af35770ca1134e15b0bb426d526
Allergy <Allergy527@gmail.com>
2023-11-25 17:01:21 +0800

    Java

--- luogu/P1035.java
+++ luogu/P1035.java
@@ -1,5 +1,18 @@
 package luogu;
 
+import java.util.Scanner;
+
 public class P1035 {
-    
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            int num = input.nextInt();
+            double Sn = 0;
+            int i = 1;
+            while (Sn <= num) {
+                Sn += 1.0 / i;
+                i += 1;
+            }
+            System.out.println(i - 1);
+        }
+    }
 }
