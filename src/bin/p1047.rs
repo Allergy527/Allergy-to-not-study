f8673dab2428d95fa2c5736a4575e3294df2ad2f
Allergy <Allergy527@gmail.com>
2023-11-26 08:33:33 +0800

    11.26

--- src/bin/p1047.rs
+++ src/bin/p1047.rs
@@ -2,26 +2,25 @@ use std::io;
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let mut input = input.split_whitespace();
-    let m: usize = input.next().unwrap().parse().unwrap();
-    let n: i32 = input.next().unwrap().parse().unwrap();
+    let mut input = input.trim().split_whitespace();
+    let (m, n): (i32, i32) = (
+        input.next().unwrap().parse().unwrap(),
+        input.next().unwrap().parse().unwrap(),
+    );
     let mut qwq = [1; 10002];
-    let mut count = 0;
     //下面是处理部分
     for _i in 0..=n - 1 {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
+
         let mut nums = input.split_whitespace();
-        let left: usize = nums.next().unwrap().parse().unwrap();
-        let right: usize = nums.next().unwrap().parse().unwrap();
+        let left: i32 = nums.next().unwrap().parse().unwrap();
+        let right: i32 = nums.next().unwrap().parse().unwrap();
+
         for j in left..=right {
-            qwq[j] = 0;
+            qwq[j as usize] = 0;
         }
     }
-    for i in 0..=m {
-        if qwq[i] == 1 {
-            count += 1;
-        }
-    }
-    println!("{}", count);
+    let count: i32 = qwq.iter().filter(|x| x == &&1).sum();
+    println!("{}", m - 10001 + count);
 }
