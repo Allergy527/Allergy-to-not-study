b5a65e3413789f066227b7be94714c635df7cb4a
Allergy <Allergy527@gmail.com>
2023-12-03 16:42:53 +0800

    12.3

--- Atcoder/12.2/C.cpp
+++ Atcoder/12.2/C.cpp
@@ -9,26 +9,24 @@
 using namespace std;
 
 void solve() {
-    ll n, m, i, j,Len=0;
+    ll n, m, i, j, Len = 0;
     cin >> n;
     vector<ll>qwq(n);
     for(auto &x : qwq)cin >> x;
     vector<ll>qvq(qwq);
     map<int, int>ck;
     sort(qvq.begin(), qvq.end());
-    ck[qvq[0]+1] = 0;
+    ck[qvq[0] + 1] = 0;
     for(i = 1;i < n;++i) {
         if(qvq[i] + 1 != qvq[i - 1] + 1) {
             ck[qvq[i] + 1] = ck[qvq[i - 1] + 1];
         }
-        ++ck[qvq[i]+1];
+        ++ck[qvq[i] + 1];
     }
     vector<ll>qaq(n);
     qaq[0] = qvq[0];
-    for(i = 1;i < n;++i)qaq[i]=qvq[i]+qaq[i - 1];
-    for(auto x : qwq) {
-        cout << qaq[n-1]-qaq[ck[x+1]]<<' ';
-    }
+    for(i = 1;i < n;++i)qaq[i] = qvq[i] + qaq[i - 1];
+    for(auto x : qwq)cout << qaq[n - 1] - qaq[ck[x + 1]] << ' ';
 
 }
 
@@ -37,6 +35,6 @@ int main() {
     int t = 1;
     //cin >> t;
     while(t--)solve();
-    cin>>t;
+    cin >> t;
     return 0;
 }
